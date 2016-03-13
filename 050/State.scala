d801c2717ea48cfee51badf0776ee42dc8cd3c44
Martino Secchi <smart91k@gmail.com>
2016-03-13 17:21:52 +0100

    exercise 05 (incomplete, 5.13 missing)

---
+++ 050/State.scala
@@ -0,0 +1,274 @@
+import scala.annotation.tailrec
+import scala.List
+import scala.collection.immutable.Stream.cons
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+object RNG {
+  // NB - this was called SimpleRNG in the book text
+
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
+      val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
+      val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
+      (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
+    }
+  }
+
+  // Exercise 1 (CB 6.1)
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val res = rng.nextInt
+    if (res._1 < 0)
+      ( - res._1 + 1, res._2 )
+    else
+      res
+  }
+  //for testing in REPL: RNG.nonNegativeInt(RNG.Simple(42))
+  //test:
+  val test1 = nonNegativeInt(Simple(42))
+
+  // Exercise 2 (CB 6.2)
+
+  def double(rng: RNG): (Double, RNG) = {
+    val pos = nonNegativeInt(rng)
+    (pos._1/(Int.MaxValue.toDouble + 1), pos._2)
+  }
+
+  val test2 = double(Simple(42))
+
+  // Exercise 3 (CB 6.3)
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val int = rng.nextInt
+    val dob = double(int._2)
+    ((int._1, dob._1), dob._2)
+  }
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val int = rng.nextInt
+    val dob = double(int._2)
+    ((dob._1, int._1), dob._2)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val dob1 = double(rng)
+    val dob2 = double(dob1._2)
+    val dob3 = double(dob2._2)
+    ((dob1._1,dob2._1,dob3._1), dob3._2)
+  }
+
+  // def boolean(rng: RNG): (Boolean, RNG) =
+  //  rng.nextInt match { case (i,rng2) => (i%2==0,rng2) }
+
+  // Exercise 4 (CB 6.4)
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    @tailrec
+    def helper(count: Int)(rng: RNG)(prog: List[Int]) : (List[Int], RNG) = {
+      if (count > 0) {
+        val r = rng.nextInt
+        helper(count - 1)(r._2)(r._1::prog)
+      } else {
+        (prog, rng)
+      }
+    }
+    helper(count)(rng)(List[Int]())
+  }
+  val test4 = ints(5)(Simple(42))
+
+  def ints2(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count > 0){
+      val r = rng.nextInt
+      val list = ints2(count-1)(r._2)
+      (r._1::list._1, list._2)
+    } else {
+      (List[Int](),rng)
+    }
+  }
+  val testints2 = ints2(5)(Simple(42))
+
+  // There is something terribly repetitive about passing the RNG along
+  // every time. What could we do to eliminate some of this duplication
+  // of effort?
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  // def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+
+  // Exercise 5 (CB 6.5)
+
+  val _double: Rand[Double] = map(nonNegativeInt)(i => i/(Int.MaxValue.toDouble +1))
+  val test5 = _double(Simple(42))
+
+  // Exercise 6 (CB 6.6)
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng => {
+      val r1 = ra(rng)
+      val r2 = rb(r1._2)
+      ( f(r1._1, r2._1), r2._2)
+    }
+  }
+
+  // this is given in the book
+
+   def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+    map2(ra, rb)((_, _))
+
+   val randIntDouble: Rand[(Int, Double)] = both(int, double)
+
+   val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+
+  val test6 = randIntDouble(Simple(42))
+
+  // Exercise 7 (6.7)
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    fs match {
+      case h::t => rng => {
+        val (hv, hs) = h(rng)
+        val (tv, ts) = sequence(t)(hs) //state chaining
+        ( hv :: tv, ts)
+      }
+      case Nil => rng => { (List[A](), rng)}
+    }
+  }
+
+  def _ints(count: Int): Rand[List[Int]] = {
+    sequence(List.fill( count )( _.nextInt ) )
+  }
+  val test7 = _ints(5)(Simple(42))
+
+  // Exercise 8 (6.8)
+
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
+    rng => {
+      val (fv, fs) = f(rng)
+      g(fv)(fs)
+    }
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    flatMap(nonNegativeInt)( i => unit(i%n) )
+  }
+
+  val test8 = nonNegativeLessThan(10)(Simple(42))
+
+  // Exercise 9 (6.9)
+
+  def _map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)( i => unit(f(i)) )
+
+  def _map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+  flatMap(ra)(a => flatMap(rb)( b => unit(f(a,b))))
+
+  val test9 = _map2(unit(1), unit(2) )( (a,b) => a+b) (Simple(42))
+
+}
+
+import State._
+
+case class State[S, +A](run: S => (A, S)) {
+
+//  "State.type doesn't take parameters error" (?)
+
+  // Exercise 10 (6.10)
+
+  def map[B](f: A => B): State[S, B] =
+    State( s => {
+      val (a,s2) = run(s)
+      (f(a),s2)
+    })
+
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    State( s => {
+      val (a,s2) = run(s)
+      val (b, s3) = sb.run(s2)
+      ( f(a,b), s3 )
+    })
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a,s2) = run(s)
+    f(a).run(s2)
+  })
+
+}
+
+object State {
+  type Rand[A] = State[RNG, A]
+
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+
+  // Exercise 10 (6.10) continued
+
+  def sequence[S,A](sas: List[State[S, A]]): State[S, List[A]] = {
+    sas match {
+      case h::t => State( s => {
+        val (hv,hs) = h.run(s)
+        val (tv, ts) = sequence(t).run(hs)
+        (hv::tv, ts)
+      })
+      case _ => State( s=> (List(),s))
+    }
+  }
+
+  // This is given in the book:
+
+  // def modify[S](f: S => S): State[S, Unit] = for {
+  //   s <- get // Gets the current state and assigns it to `s`.
+  //   _ <- set(f(s)) // Sets the new state to `f` applied to `s`.
+  // } yield ()
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+
+  def random_int :Rand[Int] =  State (_.nextInt)
+
+  // Exercise 11
+
+  def state2stream[S,A] (s :State[S,A]) (seed :S) :Stream[A] = {
+    val (aa,ss) = s.run(seed)
+    cons(aa, state2stream( unit[S,A](aa) )(ss))
+  }
+
+  val test11 = state2stream( random_int )( RNG.Simple(42))
+
+  // Exercise 12
+
+  val random_integers = state2stream( random_int )(RNG.Simple(42)).take(10).toList
+
+}
+
+sealed trait Input
+case object Coin extends Input
+case object Turn extends Input
+
+case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+object Candy {
+
+  // Exercise 13 (CB 6.11)
+
+//  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+//
+//  }
+}
+
+// vim:cc=80:foldmethod=indent:foldenable
