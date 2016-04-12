dd5773443de3471e28ee8db509a5c6f997739d88
Martino Secchi <smart91k@gmail.com>
2016-04-12 12:27:34 +0200

    090 minor changes

--- 090_miniproject/src/test/scala/fpinscala/laziness/StreamSpecMsec.scala
+++ 090_miniproject/src/test/scala/fpinscala/laziness/StreamSpecMsec.scala
@@ -25,21 +25,13 @@ class StreamSpecMsec extends FlatSpec with Checkers {
 
   import Stream._
 
-  behavior of "headOption"
-
-  // a scenario test:
-
-  it should "return None on an empty Stream (01)" in {
-    assert(empty.headOption == None)
-  }
-
   // An example generator of random finite non-empty streams
   def list2stream[A] (la :List[A]): Stream[A] = la.foldRight (empty[A]) (cons[A](_,_))
 
   // In ScalaTest we use the check method to switch to ScalaCheck's internal DSL
   def genNonEmptyStream[A] (implicit arbA :Arbitrary[A]) :Gen[Stream[A]] =
     for { la <- arbitrary[List[A]] suchThat (_.nonEmpty)}
-    yield list2stream (la)
+      yield list2stream (la)
 
   def len[A](s: Stream[A]) :Int = s.foldRight(0)((_,s) => s+1)
 
@@ -51,8 +43,18 @@ class StreamSpecMsec extends FlatSpec with Checkers {
 
   val ints = arbitrary[Int] suchThat (_ > 0)
   val streams = genNonEmptyStream[Int]
-  val exceptionStream6 = cons(throw new scala.Exception(), cons(throw new scala.Exception(), cons(throw new scala.Exception(),
-    cons(throw new scala.Exception(), cons(throw new scala.Exception(), cons(throw new scala.Exception(), empty))))))
+  val exceptionStream6 = cons(throw new scala.Exception(),
+    cons(throw new scala.Exception(), cons(throw new scala.Exception(),
+      cons(throw new scala.Exception(), cons(throw new scala.Exception(),
+        cons(throw new scala.Exception(), empty))))))
+
+  behavior of "headOption"
+
+  // a scenario test:
+
+  it should "return None on an empty Stream (01)" in {
+    assert(empty.headOption == None)
+  }
 
   // a property test:
 
@@ -83,7 +85,8 @@ class StreamSpecMsec extends FlatSpec with Checkers {
   //  - s.take(n).take(n) == s.take(n) for any Stream s and any n
   //    (idempotency)
   it should "respect idempotency (12)" in check {
-    Prop.forAll (ints, genNonEmptyStream[Int]) { (n, s) =>  forceCompareStreams(s.take(n).take(n), s.take(n)) }
+    Prop.forAll (ints, genNonEmptyStream[Int]) { (n, s) =>
+      forceCompareStreams(s.take(n).take(n), s.take(n)) }
   }
 
 //  - take(n) does not force (n+1)st head ever (even if we force all
@@ -96,13 +99,17 @@ class StreamSpecMsec extends FlatSpec with Checkers {
 
 //  - s.drop(n).drop(m) == s.drop(n+m) for any n, m (additivity)
   it should "respect additivity (21)" in check {
-    Prop.forAll(Gen.choose[Int](1, 10), Gen.choose[Int](1,10), genNonEmptyStream){(n,m,s) =>
+    Prop.forAll(Gen.choose[Int](1, 10),
+      Gen.choose[Int](1,10),
+      genNonEmptyStream){(n,m,s) =>
       forceCompareStreams(s.drop(n).drop(m), s.drop(n+m))  //the tails are forced
     }
   }
 //  - s.drop(n) does not force any of the dropped elements heads
   it should "not force dropped elements (22)" in {
-    val test = cons(throw new scala.Exception(), cons( throw new scala.Exception(), cons(1, cons(2, cons(3, empty)))))
+    val test = cons(throw new scala.Exception(),
+        cons( throw new scala.Exception(),
+        cons(1, cons(2, cons(3, empty)))))
     assert(test.drop(2).toList.equals(List(1,2,3))) //toList forces the tail
   }
 //    - the above should hold even if we force some stuff in the tail
@@ -129,8 +136,16 @@ class StreamSpecMsec extends FlatSpec with Checkers {
   // appending an empty stream should result in the starting stream s.append(Stream())==s
 
   // appending on a infinite stream should terminate ?
+  // (e.g. Stream.constant(1) is an infinite stream ore Stream.from(0))
 
-  //
+  // should not force the streams
+  it should "not force the streams" in check {
+    Prop.forAll(streams){ s => {
+       cons(1,exceptionStream6).append(s)  // doesn't pass
+      s.append(exceptionStream6)    // passes
+      true
+    } }
+  }
 
 
 }
